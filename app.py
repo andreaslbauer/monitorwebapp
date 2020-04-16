35a0e7bf5d9aa7a590717ac633be18792508b0ff
andreasalbauer <andreaslbauer@gmail.com>
2020-04-15 20:10:38 -0400

    Detect OS

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 
+# set the filename for the SQLite DB
+dbfilename = ""
 if sys.platform.startswith('win') :
     logging.info("Running on Windows")
     dbfilename = "z:\data.db"
